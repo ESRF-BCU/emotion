44cecca78a3aad63ae8b8629d0289f54ce7e558f
Matias Guijarro <matias.guijarro@esrf.fr>
2013-12-09 23:11:44 +0100

    initial commit

---
+++ emotion/__init__.py
@@ -0,0 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
+from .controller import Controller
+from .task_utils import *
+from .config import load_cfg, load_cfg_fromstring, get_axis, get_group
