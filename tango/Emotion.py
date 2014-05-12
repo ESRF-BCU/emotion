a59983cb36a51af4403191dd73c8babfce074690
blissadm@ID16NI <cyril.guilloud@esrf.fr>
2014-05-12 14:24:42 +0200

    clenaning

--- tango/Emotion.py
+++ tango/Emotion.py
@@ -488,7 +488,6 @@ class EmotionAxisClass(PyTango.DeviceClass):
              'unit': "steps/mm",
              'format': "%7.1f",
              'Display level': PyTango.DispLevel.EXPERT,
-             #'Memorized':"true"
          }],
         'Steps':
         [[PyTango.DevLong,
@@ -499,7 +498,6 @@ class EmotionAxisClass(PyTango.DeviceClass):
              'unit': "steps",
              'format': "%6d",
              'description': "number of steps in the step counter\n",
-             #'Memorized':"true"
          }],
         'Position':
         [[PyTango.DevDouble,
