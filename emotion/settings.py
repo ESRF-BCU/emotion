8d2c5273f45eab0be200cbb7aa89b3a1496936ea
Matias Guijarro <matias.guijarro@esrf.fr>
2015-05-21 23:38:57 +0200

    _set_position is now a setting

--- emotion/settings.py
+++ emotion/settings.py
@@ -37,13 +37,14 @@ def write_settings():
 class ControllerAxisSettings:
 
     def __init__(self):
-        self.setting_names = ["velocity", "position", "dial_position", "state",
+        self.setting_names = ["velocity", "position", "dial_position", "_set_position", "state",
                               "offset", "acceleration", "low_limit", "high_limit"]
         from .. import axis
         self.convert_funcs = {
             "velocity": float,
             "position": float,
             "dial_position": float,
+            "_set_position": float,
             "state": axis.AxisState,
             "offset": float,
             "low_limit": float,
